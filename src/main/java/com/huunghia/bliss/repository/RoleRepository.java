aa014f73ee1e129063815cacf102c2f81e5add7d
go-up-now <nguyenhuunghia@gmail.com>
2024-06-04 11:46:24 +0700

    update all file admin

--- src/main/java/com/huunghia/bliss/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.huunghia.bliss.repository;
-
-import com.huunghia.bliss.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
